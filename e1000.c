e1bce295e3717004f6d6aa2d150127b25cdfb91e
hongfz <hongfz16@163.com>
2018-05-21 21:31:33 +0800

    too lazy to say anything

--- e1000.c
+++ e1000.c
@@ -320,6 +320,58 @@ static void e100_tx_start(void)
   }
 }
 
+static void e100_rx_start(void)
+{
+  int i = the_e100.rx_tail % E100_RX_SLOTS;
+
+  if (the_e100.rx_tail == the_e100.rx_head)
+    panic("oops, no RFDs");
+
+  if (the_e100.rx_idle) {
+    e100_scb_wait();
+    outl(the_e100.iobase + E100_CSR_SCB_GENERAL, 
+         PADDR(&the_e100.rx[i].rfd));
+    e100_scb_cmd(E100_SCB_COMMAND_RU_START);
+    the_e100.rx_idle = 0;
+  } else {
+    e100_scb_wait();
+    e100_scb_cmd(E100_SCB_COMMAND_RU_RESUME);
+  }
+}
+
+int e100_rxbuf(struct Page *pp, unsigned int size, unsigned int offset)
+{
+  int i;
+
+  if (the_e100.rx_head - the_e100.rx_tail == E100_TX_SLOTS) {
+    cprintf("e100_rxbuf: no space\n");
+    return -E_NO_MEM;
+  }
+
+  if (size <= 4) {
+    cprintf("e100_rxbuf: weird size (%u)\n", size);
+    return -E_INVAL;
+  }
+
+  i = the_e100.rx_head % E100_TX_SLOTS;
+
+  // The first 4 bytes will hold the number of bytes recieved
+  the_e100.rx[i].rbd.rbd_buffer = page2pa(pp) + offset + 4;
+  the_e100.rx[i].rbd.rbd_size = (size - 4) & E100_SIZE_MASK;
+  the_e100.rx[i].rfd.rfa_status = 0;
+  the_e100.rx[i].rfd.rfa_control = 
+    E100_RFA_CONTROL_SF | E100_RFA_CONTROL_S;
+
+  pp->pp_ref++;
+  the_e100.rx[i].p = pp;
+  the_e100.rx[i].offset = offset;
+  the_e100.rx_head++;
+
+  e100_rx_start();
+
+  return 0;
+}
+
 void e1000_recv(void *driver, uint8_t* pkt, uint16_t *length)
 {
   // int i;
