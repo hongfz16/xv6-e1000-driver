33020b1baaa4f260311930cae0c7299c68ed7cc6
hongfz <hongfz16@163.com>
2018-05-23 16:38:23 +0800

    sync from mac to cloud server

--- pci.h
+++ pci.h
@@ -10,32 +10,60 @@
 
 #include "types.h"
 
-// Since PCI bus addresses have 8-bit for PCI bus,
-// 5-bit for device , and 3-bit for function numbers for the device
-// So a total of 2^5 devices per bus
-#define MAX_DEVICE_PER_PCI_BUS 32
+// // Since PCI bus addresses have 8-bit for PCI bus,
+// // 5-bit for device , and 3-bit for function numbers for the device
+// // So a total of 2^5 devices per bus
+// #define MAX_DEVICE_PER_PCI_BUS 32
 
 
-// http://en.wikipedia.org/wiki/PCI_Configuration_Space#Software_implementation
-// configure a PCI device by writing the address of the device's register into CONFIG_ADDRESS
-#define PCI_CONFIG_ADDRESS_IOPORT 0xCF8
-// put the data that is supposed to be written to the device into CONFIG_DATA
-#define PCI_CONFIG_DATA_IOPORT 0xCFC
+// // http://en.wikipedia.org/wiki/PCI_Configuration_Space#Software_implementation
+// // configure a PCI device by writing the address of the device's register into CONFIG_ADDRESS
+// #define PCI_CONFIG_ADDRESS_IOPORT 0xCF8
+// // put the data that is supposed to be written to the device into CONFIG_DATA
+// #define PCI_CONFIG_DATA_IOPORT 0xCFC
 
-struct pci_func;
-/**
- * We consider only one PCI bus
- */
-struct pci_bus {
-    struct pci_func *parent_bridge;
-    uint32_t busno;
-};
+// struct pci_func;
+// /**
+//  * We consider only one PCI bus
+//  */
+// struct pci_bus {
+//     struct pci_func *parent_bridge;
+//     uint32_t busno;
+// };
+
+// struct pci_func {
+//     struct pci_bus *bus;	// Primary bus for bridges
+
+//     uint32_t dev; //device number, sequentially allocated to devices on the bus
+//     uint32_t func; //function number, sequentially allocated for multi-func devices
+
+//     uint32_t dev_id;
+//     uint32_t dev_class;
+
+//     uint32_t reg_base[6];
+//     uint32_t reg_size[6];
+//     uint8_t irq_line;
+//     uint8_t irq_pin;
+// };
+
+// // PCI driver container
+// struct pci_driver {
+// 	uint32_t key1, key2;
+
+// 	int (*attachfn) (struct pci_func *pcif);
+// };
+
+// int pci_init(void);
+
+enum { pci_res_bus, pci_res_mem, pci_res_io, pci_res_max };
+
+struct pci_bus;
 
 struct pci_func {
-    struct pci_bus *bus;	// Primary bus for bridges
+    struct pci_bus *bus;    // Primary bus for bridges
 
-    uint32_t dev; //device number, sequentially allocated to devices on the bus
-    uint32_t func; //function number, sequentially allocated for multi-func devices
+    uint32_t dev;
+    uint32_t func;
 
     uint32_t dev_id;
     uint32_t dev_class;
@@ -43,16 +71,14 @@ struct pci_func {
     uint32_t reg_base[6];
     uint32_t reg_size[6];
     uint8_t irq_line;
-    uint8_t irq_pin;
 };
 
-// PCI driver container
-struct pci_driver {
-	uint32_t key1, key2;
-
-	int (*attachfn) (struct pci_func *pcif);
+struct pci_bus {
+    struct pci_func *parent_bridge;
+    uint32_t busno;
 };
 
-int pci_init(void);
+int  pci_init(void);
+void pci_func_enable(struct pci_func *f);
 
 #endif
